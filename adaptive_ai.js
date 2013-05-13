df0dfdcbff1eaa10be981d33e9063de170ab5298
John Oliva <joliva@olivalabs.com>
2013-05-13 14:13:36 +0000

    Implemented Organism.Sensor class and part of the Organism class for the module.

--- adaptive_ai.js
+++ adaptive_ai.js
@@ -434,10 +434,395 @@ Organism = function(organism) {
 		this._stateCount = 0;
 		this._sensorCount = 0;
 		this._currentState = 0;
+		this._orgGenome = new Genome();
 	} else {
 		this.copy(organism);
 	}
 }
+
+Organism.prototype.copy = function(organism) {
+	this._states = [];
+	this._sensors = [];
+	this._stateCount  = organism._stateCount;
+	this._sensorCount = organism._sensorCount;
+
+	States  = new State  [StateCount];
+	Sensors = new Sensor [SensorCount];
+
+	for (var i=0, l=this._stateCount; i<l; i++) {
+		this._states.push(new Organism.State(organism._states[i]);
+	}
+
+	for (var i=0, l=this._sensorCount; i<l; i++) {
+		this._sensors.push(new Organism.Sensor(organism._sensors[i]);
+	}
+
+	this._orgGenome.copy(organism._orgGenome);
+
+	return this;
+}
+
+Organism.prototype.add = function(organism) {
+	temp = new Organism();
+
+	if (this._stateCount != organism._stateCount || this._sensorCount != organism._sensorCount) {
+		throw new RangeError('Incompatible # of states/sensors for copying Organsim');
+	}
+
+	temp.copy(this);
+
+	temp._orgGenome = this._orgGenome.add(organism._orgGenome);
+
+	// Mutate the offspring's genome:
+	temp.mutate ();
+
+	return temp;
+}
+
+Organism.prototype.getStateName = function(idx) {
+	if (idx < 0 || idx >= this._stateCount)
+		return '';
+
+	return this._states[idx]._name;
+}
+
+Organism.prototype.setStateName = function(idx, name) {
+	if (idx < 0 || idx >= this._stateCount)
+		return false;
+
+	this._states[idx]._setName(name);
+
+	return true;
+}
+
+Organism.prototype.setStateCount = function(count) {
+	if (count < 0)
+		return false;
+
+	// One chromosome for each state:
+	this._stateCount = count;
+
+	this._states=[];
+
+	for (var i=0, l=this._stateCount; i<l; i++) {
+		this.states.push(new Organism.State());
+	}
+
+	if (this._orgGenome.setChromosomeCount(this._stateCount) === false) {
+		return false;
+	}
+
+	// StateCount genes for each state:
+	for (var i=0, l=this._stateCount; i<l; i++) {
+		var chrom = this._orgGenome.getChromosome(i);
+		chrom.setGeneCount(this._stateCount);
+	}
+
+	// Now set sensor count since it depends on state count:
+	return this.setSensorCount(this._sensorCount);
+}
+
+/*
+int Organism::GetStateCount () const {
+	return StateCount;
+}
+
+int Organism::GetStateIndex (std::string Name) const {
+	for (int i = 0; i < StateCount; i++) {
+		if (Name == States [i].Name) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+float Organism::GetSensorValue (std::string Name) const {
+	for (int i = 0; i < SensorCount; i++) {
+		if (Name == Sensors [i].Name)
+			return Sensors [i].Value;
+	}
+
+	return 0.0F;
+}
+
+float Organism::GetSensorValue (int Index) const {
+	if (Index < 0 || Index >= SensorCount)
+		return 0.0F;
+
+	return Sensors [Index].Value;
+}
+
+bool Organism::SetSensorValue (std::string Name, float Value) {
+	for (int i = 0; i < SensorCount; i++) {
+		if (Name == Sensors [i].Name) {
+			Sensors [i].SetValue (Value);
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool Organism::SetSensorValue (int Index, float Value) {
+	if (Index < 0 || Index >= SensorCount)
+		return false;
+
+	Sensors [Index].SetValue (Value);
+
+	return true;
+}
+
+std::string Organism::GetSensorName (int Index) const {
+	if (Index < 0 || Index >= SensorCount)
+		return NULL;
+
+	return Sensors [Index].Name;
+}
+
+bool Organism::GetSensorName (int Index, std::string* Name) const {
+	if (Index < 0 || Index >= SensorCount)
+		return false;
+
+	*Name = Sensors [Index].Name;
+
+	return true;
+}
+
+bool Organism::SetSensorName (int Index, std::string Name) {
+	if (Index < 0 || Index >= SensorCount)
+		return false;
+
+	Sensors [Index].SetName (Name);
+
+	return true;
+}
+
+bool Organism::SetSensorCount (int Count) {
+
+	if (Count < 0) {
+		return false;
+	}
+	
+	SensorCount = Count;
+
+	if (Count > 0) {
+		if (Sensors == NULL) {
+			Sensors = new Sensor [SensorCount];
+		}
+	}
+
+	for (int i = 0; i < StateCount; i++) {
+		Chromosome &Chrom = OrgGenome.GetChromosome (i);
+
+		for (int j = 0; j < StateCount; j++) {
+			Gene &G = Chrom.GetGene (j);
+
+			// Allocate enough room for each coefficient
+			// (base + sensor coeff's):
+			G.SetLength (1 + SensorCount);
+		}
+	}
+
+	return true;
+}
+
+int Organism::GetSensorCount () const {
+	return SensorCount;
+}
+
+int Organism::GetSensorIndex (std::string Name) const {
+	for (int i = 0; i < SensorCount; i++) {
+		if (Name == Sensors [i].Name) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+bool Organism::SetTransition (int Index1, int Index2, float BaseChance, const float *SensorCoeff) {
+	if (Index1 < 0 || Index1 >= StateCount || Index2 < 0 || Index2 >= StateCount)
+		return false;
+
+	Chromosome &Chrom = OrgGenome.GetChromosome (Index1);
+
+	Gene &G = Chrom.GetGene (Index2);
+
+	G.SetElement (0, BaseChance);
+
+	for (int i = 0; i < SensorCount; i++)
+		G.SetElement (1 + i, SensorCoeff [i]);
+
+	return true;
+}
+
+int Organism::GetCurrentState () const {
+	return CurrentState;
+}
+
+bool Organism::GetCurrentState (std::string* Name) const {
+	*Name = States [CurrentState].Name;
+	return true;
+}
+
+bool Organism::SetCurrentState (std::string Name) {
+	for (int i = 0; i < StateCount; i++) {
+		if (States [i].Name == Name) {
+			CurrentState = i;
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool Organism::SetCurrentState (int Index) {
+	if (Index < 0 || Index >= StateCount)
+		return false;
+
+	CurrentState = Index;
+
+	return true;
+}
+
+bool Organism::Mutate () {
+	return OrgGenome.Mutate ();
+}
+
+bool Organism::MutateMutationFactors (float Chance, float Rate) {
+	return OrgGenome.MutateMutationFactors (Chance, Rate);
+}
+
+bool Organism::UpdateState () {
+	float *Prob = new float [StateCount];
+
+	float TotalProb = 0.0F;
+
+	int i;
+
+	Chromosome &Chrom = OrgGenome.GetChromosome (CurrentState);
+
+	for (i = 0; i < StateCount; i++) {
+		Gene &G = Chrom.GetGene (i);
+
+		// Base chance:
+		Prob [i]  = G.GetElement (0);
+
+		TotalProb  += Prob [i];
+
+		// Sensor coefficients:
+		for (int j = 1; j <= SensorCount; j++) {
+			float p = G.GetElement (j) * Sensors [j - 1].Value;
+			
+			Prob [i] += p;
+
+			TotalProb  += p;
+		}
+	}
+
+	// Normalize probability:
+	for (i = 0; i < StateCount; i++)
+		Prob [i] /= TotalProb;
+
+	// cdf - cumulative distribution function
+	for (i = StateCount - 1; i >= 0; i--) {
+		for (int j = 0; j < i; j++)
+			Prob [i] += Prob [j];
+	}
+
+	float Choice = Random ();
+
+	int NextState = CurrentState;
+
+	for (i = 0; i < StateCount; i++) {
+		if (Choice <= Prob [i]) {
+			NextState = i;
+
+			break;
+		}
+	}
+
+	CurrentState = NextState;
+
+	delete [] Prob;
+
+	return true;
+}
+
+bool Organism::Save (std::fstream &File) const {
+	// File format:
+	//			 StateCount			sizeof (int)
+	//			 SensorCount		  sizeof (int)
+	//			 CurrentState		 sizeof (int)
+	//			 States				 sizeof (State) * StateCount
+	//			 Sensors				sizeof (Sensor) * SensorCount
+	//			 OrgGenome			 varies
+	File.write ((const char *) &StateCount,	sizeof (int));
+	File.write ((const char *) &SensorCount,  sizeof (int));
+	File.write ((const char *) &CurrentState, sizeof (int));
+	
+	// Save states:
+	int i;
+	for (i = 0; i < StateCount; i++) {
+		if (!States [i].Save (File))
+			return false;
+	}
+
+	// Save sensors:
+	for (i = 0; i < SensorCount; i++) {
+		if (!Sensors [i].Save (File))
+			return false;
+	}
+
+	// Save the genome:
+	if (!OrgGenome.Save (File))
+		return false;
+
+	if (!File.good ())
+		return false;
+
+	return true;
+}
+
+bool Organism::Load (std::fstream &File) {
+	File.read ((char *) &StateCount,	sizeof (int));
+	File.read ((char *) &SensorCount,  sizeof (int));
+	File.read ((char *) &CurrentState, sizeof (int));
+	
+	// Allocate memory for states:
+	delete [] States;
+	States = new State [StateCount];
+
+	// Load states:
+	int i;
+	for (i = 0; i < StateCount; i++) {
+		if (!States [i].Load (File))
+			return false;
+	}
+
+	// Allocate memory for the sensors:
+	delete [] Sensors;
+	Sensors = new Sensor [SensorCount];
+
+	// Load sensors:
+	for (i = 0; i < SensorCount; i++) {
+		if (!Sensors [i].Load (File))
+			return false;
+	}
+
+	// Load the genome:
+	if (!OrgGenome.Load (File))
+		return false;
+
+	if (!File.good ())
+		return false;
+
+	return true;
+}
+*/
 
 // --------------------------------------------------------------------------
 //	Organism: State
@@ -470,6 +855,47 @@ Organism.State.prototype.load = function(fstream) {
 // --------------------------------------------------------------------------
 //	Organism: Sensor
 // --------------------------------------------------------------------------
+
+Organism.Sensor = function(sensor) {
+	if (sensor === undefined) {
+		this._value = 0.0;
+		this._name = 'NOT_SET';
+	} else {
+		this.copy(sensor);
+	}
+}
+
+Organism.Sensor.prototype.setName = function(newName) {
+	this._name = newName;
+	return true;
+}
+
+Organism.Sensor.prototype.getName = function() {
+	return this._name
+}
+
+Organism.Sensor.prototype.setValue = function(value) {
+	this._value = value;
+
+	return true;
+}
+
+Organism.Sensor.prototype.getValue = function() {
+	return this._value;
+}
+
+Organism.Sensor.prototype.copy = function(sensor) {
+	this._name = sensor._name;
+	this._value = sensor._value;
+
+	return this;
+}
+
+Organism.Sensor.prototype.save = function(fstream) {
+}
+
+Organism.Sensor.prototype.load = function(fstream) {
+}
 
 // --------------------------------------------------------------------------
 exports.GA={};
